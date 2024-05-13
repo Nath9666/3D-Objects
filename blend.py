0ab33777ffe38f5ea45f88167ae70a46f1398f12
Nathan <93925029+Nath9666@users.noreply.github.com>
2024-05-13 13:54:44 +0200

    ajout de base

--- blend.py
+++ blend.py
@@ -12,7 +12,7 @@ def trouver_fichiers_blend(repertoire, exeption=None):
     fichiers_blend = []
     for dossier, sous_dossiers, fichiers in os.walk(repertoire):
         for fichier in fichiers:
-            if fichier.endswith(".blend"):
+            if fichier.endswith(".blend") and dossier != exeption and os.path.islink(dossier):
                 chemin_fichier = os.path.join(dossier, fichier).replace("\\", "/")
                 date_creation = time.ctime(os.path.getctime(chemin_fichier))
                 date_modification = time.ctime(os.path.getmtime(chemin_fichier))
@@ -24,13 +24,12 @@ if __name__ == "__main__":
     print("Choisissez un disque : ")
     rep = input()
     print("Recherche des fichiers blend... dans le disque", rep)
-    fichiers_blend = trouver_fichiers_blend(rep)
+    # trouve les fichiers blend dans le repertoire exepter le dossier ou est lancer le programme
+    fichiers_blend = trouver_fichiers_blend(rep, os.getcwd())
 
     print("Ecriture des fichiers blend dans un fichier JSON...")
 
     # ecrit dans un fichier JSON la liste des fichiers blend
     with open("fichiers_blend.json", "w") as f:
-        for fichier_blend in fichiers_blend:
-                print(fichier_blend)
-                json.dump(fichier_blend, f)
-                f.write("\n")
+            json.dump(fichiers_blend, f)
+            f.write("\n")
